a6e41f03571b7bf6ead29a35a5210bf1de8ad363
francisco.malison.da.silva.alves <gsmalison98@gmail.com>
2026-09-24 13:52:47 -0300

    Fazendo o esqueleto de novas funcionalidades e limpando o main

---
+++ src/Jogodobicho/Apostas.java
@@ -0,0 +1,87 @@
+package Jogodobicho;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Apostas {
+    private int [] aposta;
+    private boolean apostaRealizada = false;
+    public final String [] tabelaDosBichos = new String[]
+            {"1-Avestruz", "2-Águia", "3-Burro", "4-Borboleta", "5-Cachorro",
+                    "6-Cabra", "7-Carneiro", "8-Camelo", "9-Cobra", "10-Coelho",
+                    "11-Cavalo", "12-Elefante", "13-Galo", "14-Gato", "15-Jacaré",
+                    "16-Leão", "17-Macaco", "18-Porco", "19-Pavão", "20-Peru",
+                    "21-Touro", "22-Tigre", "23-Urso", "24-Veado", "25-Vaca"
+            };
+
+    public void setAposta(String nomes) {
+        String[] nuBicho = nomes.split(" ");
+
+        if (nuBicho.length == 5) {
+            int[] numerosApostados;
+            numerosApostados = new int[5];
+            for (int i = 0; i < nuBicho.length; i++) {
+                int numero = Integer.parseInt(nuBicho[i]);
+
+                if (numero < 1 || numero > 25) {
+                    System.out.println("Numero Invalido apostado");
+                    apostaRealizada = false;
+                    break;
+                }
+                if (numerosApostados[0] == numero || numerosApostados[1] == numero || numerosApostados[2] == numero || numerosApostados[3] == numero || numerosApostados[4] == numero) {
+                    System.out.println("Numeros repetidos na Sequência");
+                    apostaRealizada = false;
+                    break;
+                }
+                numerosApostados[i] = numero;
+                apostaRealizada = true;
+
+            }
+
+            if (apostaRealizada) {
+                Arrays.sort(numerosApostados);
+                aposta = numerosApostados;
+                System.out.println("Aposta realizada com sucesso");
+            }
+
+        }else {
+            System.out.println("quantidade de numeros apostados invalidos!");
+        }
+    }
+    public void fazerApostas(){}
+    public String getAposta(){
+        String resposta ="";
+        if(apostaRealizada){
+            for (int j : aposta) {
+                resposta += getBicho(j) + " ";
+            }
+        }else {
+            resposta = "Faça a aposta antes!";
+        }
+        return resposta.trim();
+    }
+
+    public String getBicho(int numeroDoBicho){
+        if (numeroDoBicho < 1 || numeroDoBicho > 25){
+            return "Número invalido inserido";
+        }
+        String bicho = tabelaDosBichos[numeroDoBicho-1];
+        return bicho;
+    }
+    public String [] verTabela(){
+        return tabelaDosBichos;
+    }
+    public String verResultado(){
+        Random random = new Random();
+        if (apostaRealizada){
+            apostaRealizada = false;
+            int numero = 1 + random.nextInt(25);
+            if (aposta[0] == numero || aposta[1] == numero || aposta[2] == numero || aposta[3] == numero || aposta[4] == numero){
+                return "Deu "+ getBicho(numero) + ", Vôce Ganhou!!!";
+            }
+            return "Deu " + getBicho(numero) + ", Vôce perdeu";
+        }else{
+            return "faça uma aposta";
+        }
+    }
+}
--- src/Jogodobicho/InterfaceJogo.java
+++ src/Jogodobicho/InterfaceJogo.java
@@ -1,13 +1,15 @@
+package Jogodobicho;
+
 import java.util.Scanner;
 public class InterfaceJogo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Apostas apostas = new Apostas();
 
-        String menu = "1-Apostar;\n"+
-                      "2-Ver Aposta;\n"+
-                      "3-Olhar Bicho pelo numero;\n"+
-                      "4-Sair";
+        String menu = "1-Apostar;\n" +
+                "2-Ver Aposta;\n" +
+                "3-Olhar Bicho pelo numero;\n" +
+                "4-Sair";
         final int APOSTAR = 1;
         final int VERAPOSTA = 2;
         final int OLHARBICHO = 3;
@@ -16,8 +18,8 @@ public class InterfaceJogo {
         int op;
 
         do {
-            op = leInt(menu,sc);
-            switch (op){
+            op = leInt(menu, sc);
+            switch (op) {
                 case APOSTAR:
                     fazerAposta(sc, apostas);
                     break;
@@ -25,47 +27,39 @@ public class InterfaceJogo {
                     verAposta(apostas);
                     break;
                 case OLHARBICHO:
-                    verBicho(sc,apostas);
+                    verBicho(sc, apostas);
                     break;
                 case SAIR:
                     break;
                 default:
                     System.out.println("Opção Inválida!");
             }
-        }while(op != SAIR);
+        } while (op != SAIR);
         sc.close();
     }
 
-    private static void fazerAposta(Scanner sc, Apostas apostas){
+    private static void fazerAposta(Scanner sc, Apostas apostas) {
         System.out.println("faça a aposta de 5 numeros entre 1 e 25, cada um separado por expaço");
         String entrada = sc.nextLine();
-        String [] Apostados = entrada.split(" ");
-
-        if(Apostados.length != 5){
-            System.out.println("quantidade de numeros apostados invalidos!");
-        }else{
-            apostas.setAposta(Apostados);
-        }
+        apostas.setAposta(entrada);
     }
+
     private static void verAposta(Apostas apostas){
         String bichosApostados = apostas.getAposta();
         System.out.println(bichosApostados);
 
     }
+
     private static void verBicho(Scanner sc, Apostas apostas){
         System.out.println("Digite um numero");
         int numeroDoBicho = sc.nextInt();
-        if(numeroDoBicho < 1 || numeroDoBicho > 25){
-            System.out.println("Numero invalido");
-        }else {
-            String bicho = apostas.getBicho(numeroDoBicho);
-            System.out.println(bicho);
+        System.out.println(apostas.getBicho(numeroDoBicho));
         }
-    }
-    private static int leInt(String msg, Scanner scam){
+
+    private static int leInt(String msg, Scanner sc){
         System.out.println(msg);
-        int op = scam.nextInt();
-        scam.nextLine();
+        int op = sc.nextInt();
+        sc.nextLine();
         return op;
     }
 }
